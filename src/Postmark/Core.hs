8989221639178b61f10a551311333ccbfa782dcb
Mark Hibberd <mark@hibberd.id.au>
2012-09-09 22:36:50 +1000

    correct response code

--- src/Postmark/Core.hs
+++ src/Postmark/Core.hs
@@ -36,25 +36,30 @@ responder (Response status _ _ body) =
   let bt = LT.toStrict . LE.decodeUtf8 $ body
    in case status of
     -- FIX format date
-    (Status 200 _) -> withJson body (\(PostmarkResponseSuccessData ident at to) -> PostmarkResponseSuccess ident undefined to)
+    (Status 200 _) -> withJson 200 body (\(PostmarkResponseSuccessData ident at to) -> PostmarkResponseSuccess ident undefined to)
     (Status 401 _) -> PostmarkResponseUnauthorized
-    (Status 422 _) -> withJson body (\(PostmarkResponseErrorData code message) -> PostmarkResponseUnprocessible (toPostmarkError code) message)
+    (Status 422 _) -> withJson 422 body (\(PostmarkResponseErrorData code message) -> PostmarkResponseUnprocessible (toPostmarkError code) message)
     (Status 500 _) -> PostmarkResponseServerError bt
     (Status c _) -> PostmarkResponseInvalidResponseCode c bt
 
-withJson :: FromJSON a => BL.ByteString -> (a -> PostmarkResponse) -> PostmarkResponse
-withJson bs f =
-  let bt = LT.toStrict . LE.decodeUtf8 $ bs
+withJson :: FromJSON a => Int -> BL.ByteString -> (a -> PostmarkResponse) -> PostmarkResponse
+withJson code bs f =
+  let bt = toText bs
   in case parseJson bs of
-    Left (Left msg) -> PostmarkResponseJsonSyntaxError 200 msg bt
-    Left (Right msg) -> PostmarkResponseJsonFormatError 200 msg bt
+    Left (Left msg) -> PostmarkResponseJsonSyntaxError code msg bt
+    Left (Right msg) -> PostmarkResponseJsonFormatError code msg bt
     Right a -> f a
 
 parseJson :: FromJSON a => BL.ByteString -> Either (Either Text Text) a
 parseJson bs =
-  let strict = B.concat . BL.toChunks $ bs
-   in case parseOnly json strict of
-      Left msg -> Left (Left . pack $ msg)
-      Right j -> case fromJSON j of
-        (Error msg') -> Left (Right . pack $ msg')
-        (Success a) -> Right a
+  case parseOnly json (toStrictBS bs) of
+    Left msg -> Left (Left . pack $ msg)
+    Right j -> case fromJSON j of
+      (Error msg') -> Left (Right . pack $ msg')
+      (Success a) -> Right a
+
+toText :: BL.ByteString -> Text
+toText = LT.toStrict . LE.decodeUtf8
+
+toStrictBS :: BL.ByteString -> B.ByteString
+toStrictBS = B.concat . BL.toChunks
