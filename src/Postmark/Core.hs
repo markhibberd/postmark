8d8a360a2edfe29cfcd6555c640174c720a6c5ac
markhibberd <mark@hibberd.id.au>
2013-01-27 12:56:35 +1100

    Use network api responders.

--- src/Postmark/Core.hs
+++ src/Postmark/Core.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Postmark.Core (sendEmail) where
 
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import Data.Attoparsec.Lazy
 import Data.Aeson
 import Data.Text
 import Data.Text.Encoding
@@ -27,38 +25,23 @@ sendEmail req =
       , ("X-Postmark-Server-Token", encodeUtf8 $  postmarkToken req)
       ] <>
     setJson (postmarkEmail req)
-  ) responder
+  ) (basicResponder responder)
 
-responder :: Response BL.ByteString -> PostmarkResponse
-responder (Response (Status 200 _) _ _ body) =
+responder :: Int -> BL.ByteString -> PostmarkResponse
+responder 200 body =
   withJson 200 body (\(PostmarkResponseSuccessData ident at to) -> PostmarkResponseSuccess ident at to)
-responder (Response (Status 401 _) _ _ _) =
+responder 401 _ =
   PostmarkResponseUnauthorized
-responder (Response (Status 422 _) _ _ body) =
+responder 422 body =
   withJson 422 body (\(PostmarkResponseErrorData code message) -> PostmarkResponseUnprocessible (toPostmarkError code) message)
-responder (Response (Status 500 _) _ _ body) =
+responder 500 body =
   PostmarkResponseServerError (toText body)
-responder (Response (Status c _) _ _ body) =
+responder c body =
   PostmarkResponseInvalidResponseCode c (toText body)
 
 withJson :: FromJSON a => Int -> BL.ByteString -> (a -> PostmarkResponse) -> PostmarkResponse
-withJson code bs f =
-  let bt = toText bs
-  in case parseJson bs of
-    Left (Left msg) -> PostmarkResponseJsonSyntaxError code msg bt
-    Left (Right msg) -> PostmarkResponseJsonFormatError code msg bt
-    Right a -> f a
-
-parseJson :: FromJSON a => BL.ByteString -> Either (Either Text Text) a
-parseJson bs =
-  case parseOnly json (toStrictBS bs) of
-    Left msg -> Left (Left . pack $ msg)
-    Right j -> case fromJSON j of
-      (Error msg') -> Left (Right . pack $ msg')
-      (Success a) -> Right a
+withJson code body f =
+  parseBodyWith body (\msg -> PostmarkResponseJsonSyntaxError code msg (toText body)) (\msg -> PostmarkResponseJsonFormatError code msg (toText body)) f
 
 toText :: BL.ByteString -> Text
 toText = LT.toStrict . LE.decodeUtf8
-
-toStrictBS :: BL.ByteString -> B.ByteString
-toStrictBS = B.concat . BL.toChunks
