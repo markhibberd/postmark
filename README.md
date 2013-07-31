4112535112f78651083ea2214316223c662f6e34
markhibberd <mark@hibberd.id.au>
2013-07-31 22:40:39 +1000

    Doc, version bump for release.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 Library for postmarkapp.com HTTP Api
 
+To get start see some examples in the [Network.Api.Postmark.Tutorial](http://hackage.haskell.org/packages/archive/postmark/0.0.2/doc/html/Network-Api-Postmark-Tutorial.html).
+
+To experiment with a live demo try:
+```
+git clone https://github.com/apiengine/postmark
+cd postmark
+cabal install --only-dependencies &&  cabal configure -f demo  && cabal build
+./dist/build/postmark-demo/postmark-demo
+```
+
+Issues can be reported at <https://github.com/apiengine/postmark/issues>.
+
+If you need help or have any questions, get in contact:
+ * [@markhibberd](https://twitter.com/markhibberd) on twitter
+ * `[mth]` on freenode #haskell/#scalaz
+ * mark@hibberd.id.au on email
--- src/Network/Api/Postmark.hs
+++ src/Network/Api/Postmark.hs
@@ -1,11 +1,23 @@
 -- |
--- Module:      Network.Postmark
+-- Module:      Network.Api.Postmark
 -- Copyright:   (c) 2012 Mark Hibberd
 -- License:     BSD3
 -- Maintainer:  Mark Hibberd <mark@hibberd.id.au>
 -- Portability: portable
 --
--- Library for postmarkapp.com HTTP Api
+-- Library for postmarkapp.com HTTP Api.
+--
+-- To get start see some examples in the 'Network.Api.Postmark.Tutorial' module at <http://hackage.haskell.org/packages/archive/postmark/0.0.2/doc/html/Network-Api-Postmark-Tutorial.html>.
+--
+-- Source and more information can be found at <https://github.com/apiengine/postmark>.
+--
+-- To experiment with a live demo try:
+-- > $ git clone https://github.com/apiengine/postmark
+-- > $ cd postmark
+-- > $ cabal install --only-dependencies &&  cabal configure -f demo  && cabal build
+-- > $ ./dist/build/postmark-demo/postmark-demo
+--
+-- Issues can be reported at <https://github.com/apiengine/postmark/issues>.
 --
 module Network.Api.Postmark (module X) where
 
